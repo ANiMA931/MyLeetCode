8dd86b92fd8bb9d9792d8479576e2c7583484702
ANiMA931 <zevanima931@gmail.com>
2022-02-21 00:17:53 +0800

    loading first time

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+int main()
+{
+    std::cout<<"HELLO WORLD."<<std::endl;
+    system("pause");
+    return 0;
+}
